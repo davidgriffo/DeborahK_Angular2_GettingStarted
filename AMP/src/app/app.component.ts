c7528cb63d130f070da41b52d8264f41faec8e4a
david griffiths <davidgriffo@gmail.com>
2019-10-21 16:07:19 +1000

    small change to app component

--- AMP/src/app/app.component.ts
+++ AMP/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started for david';
+  title = 'Angular: Getting Started';
+  user = 'David'
 }
